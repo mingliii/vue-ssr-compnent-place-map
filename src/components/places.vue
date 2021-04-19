94595daf4471be7d363eb42d12f5ef9a541097a4
Ming Li <li.ming116@gmail.com>
2021-04-19 16:45:53 +0100

    backend loads 30 and fronend loads more

--- src/components/places.vue
+++ src/components/places.vue
@@ -3,21 +3,24 @@
         <h1>{{ name }}</h1>
         <div>
             <c-simple-grid minChildWidth="250px" spacing="30px">
-                <card v-for="place in this.places.slice(0,30)" :key="place.id.value" :place="place"/>
+                <card v-for="place in this.places.slice(0, this.numOfPlaces)" :key="place.id.value" :place="place"/>
             </c-simple-grid>
+        </div>
+        <div style="margin: 0 auto;padding: 10px;text-align: center">
+            <c-button variant-color="green" @click="loadMore">Show more</c-button>
         </div>
     </div>
 </template>
 
 <script>
-import {CSimpleGrid} from '@chakra-ui/vue'
+import {CSimpleGrid, CButton} from '@chakra-ui/vue'
 import Card from './lib/card.vue'
 import {mapState} from 'vuex';
 
 export default {
     name: 'Places',
     components: {
-        CSimpleGrid, Card
+        CSimpleGrid, CButton, Card
     },
     computed: {
         ...mapState(['places']),
@@ -25,6 +28,7 @@ export default {
     data() {
         return {
             name: 'Places page',
+            numOfPlaces: 30
         }
     },
     serverPrefetch() {
@@ -33,6 +37,16 @@ export default {
     created() {
         if (this.places.length === 0) {
             return this.$store.dispatch('fetchPlaces');
+        }
+    },
+    methods: {
+        loadMore() {
+            console.log('this.numOfPlaces', this.numOfPlaces)
+            if (this.places.length > 30 + this.numOfPlaces) {
+                this.numOfPlaces = 30 + this.numOfPlaces;
+            } else {
+                this.numOfPlaces = this.places.length;
+            }
         }
     }
 };
